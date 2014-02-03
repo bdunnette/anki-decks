f302578d1167d8e83201600ea58adfb099626cf7
Brian Dunnette <dunn0172@umn.edu>
2014-02-03 11:33:17 -0600

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('ankiDecksApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/decks/collections.json').success(function (data) {$scope.decks = data});
+    $scope.gridOptions = { 
+      data: 'decks',
+      columnDefs: [
+          {field:'title', displayName:'Title', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><span ng-cell-text><a href="{{row.getProperty(\'anki_package\')}}">{{row.getProperty(col.field)}}</a></span></div>'}, 
+          {field:'modified_date', displayName:'Last Updated', width: '20%'}
+      ],
+      sortInfo: { 
+          fields: ['modified_date','title'], 
+          directions: ['desc', 'asc']
+      }
+    };
+  });
