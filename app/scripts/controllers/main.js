f465454806047b24bee28826babcd07a8e4b0bd9
Brian Dunnette <dunn0172@umn.edu>
2014-02-03 11:44:35 -0600

    correcting urls

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,11 @@
 
 angular.module('ankiDecksApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $http.get('/decks/collections.json').success(function (data) {$scope.decks = data});
+    $http.get('./decks/collections.json').success(function (data) {$scope.decks = data});
     $scope.gridOptions = { 
       data: 'decks',
       columnDefs: [
-          {field:'title', displayName:'Title', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><span ng-cell-text><a href="{{row.getProperty(\'anki_package\')}}">{{row.getProperty(col.field)}}</a></span></div>'}, 
+          {field:'title', displayName:'Title', cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><span ng-cell-text><a href="./decks/{{row.getProperty(\'anki_package\')}}">{{row.getProperty(col.field)}}</a></span></div>'}, 
           {field:'modified_date', displayName:'Last Updated', width: '20%'}
       ],
       sortInfo: { 
